2044cba309a17005b3ba8538dcee964b4ad2f223
infantito <danielinfantito61@gmail.com>
2021-10-26 21:50:07 -0500

    feat: add constants

---
+++ src/constants/colors.ts
@@ -0,0 +1,6 @@
+const colors = {
+  primary: '#C2185B',
+  accent: '#FFC107',
+} as const
+
+export default colors
---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export * from './common'
+
+export * as REGEX from './regex'
+
+export { default as COLORS } from './colors'
